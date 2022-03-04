fd561185c1d6eeee19d9aeae16f9541287ec56d1
kingyond <900483@qq.com>
2022-03-04 09:45:37 +0800

    init

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,11 @@
-#! /bin/sh
+#!/bin/bash
 
-arr=('docker' 'entware')
+array=("docker" "entware")
 
-for item in ${arr[*]}
+for element in "${array[@]}"
 do
-  cd $GITHUB_WORKSPACE/wdpksrc/wdpk/$item
-  ./build.sh
-  cp $SRC_TAR $GITHUB_WORKSPACE/output/bin/$SRC_TAR
+    echo "$element"
+    cd $GITHUB_WORKSPACE/wdpksrc/wdpk/$element
+    ./build.sh
+    cp $SRC_TAR $GITHUB_WORKSPACE/output/bin/$SRC_TAR
 done
